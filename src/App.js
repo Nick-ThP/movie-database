00af302bf5f60589cb97dc6dc554b6a6295f4562
Nicklas <nicklas_tp@hotmail.com>
2022-03-07 23:34:35 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles/App.sass';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const key = process.env.REACT_APP_MOVIE_DATABASE_API_KEY
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [term, setTerm] = useState([]);
+
+  const searchMovies = (term) => {
+    axios.get(`http://www.omdbapi.com/?apikey=${key}&s=${term}`)
+      .then((response) => {
+          setMovies(response.data.Search)
+      });
+  };
+
+  console.log(movies)
+
+  useEffect(() => {
+    searchMovies(term)
+  }, [term])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1>Movie Database</h1>
+      <div className={styles.search}>
+        <input
+          type="text"
+          placeholder="Search for movies"
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </div>
+      <div className={styles.wrapper}>
+      </div>
     </div>
   );
 }
