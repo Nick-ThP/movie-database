e36a3ef405a879245568e9794acc52dc9eb06aab
Nicklas <nicklas_tp@hotmail.com>
2022-03-08 17:16:16 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import styles from './styles/App.module.sass';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './components/MovieCard';
+import SearchIcon from "./search.svg";
 
 const key = process.env.REACT_APP_MOVIE_DATABASE_API_KEY
 
 function App() {
 
   const [movies, setMovies] = useState([]);
-  const [term, setTerm] = useState([]);
+  const [term, setTerm] = useState("");
 
   const searchMovies = (term) => {
     axios.get(`http://www.omdbapi.com/?apikey=${key}&s=${term}`)
@@ -23,18 +25,47 @@ function App() {
   }, [term])
 
   return (
-    <div className={styles.container}>
-      <h1>Movie Database</h1>
-      <div className={styles.search}>
-        <input
-          type="text"
-          placeholder="Search for movies"
-          onChange={(e) => setTerm(e.target.value)}
-        />
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Movie Database</h1>
+        <div className={styles.search}>
+          <input 
+            className={styles.searchBar}
+            type="text"
+            placeholder="search for movies here"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <div className={styles.icon}>
+            <img 
+              src={SearchIcon} 
+              alt="search"
+            />
+          </div>
+        </div>
       </div>
+
+      <div>
+        <div className={styles.skewed}></div>
+      </div>
+        
       <div className={styles.wrapper}>
+        {movies?.length > 0 
+          ? (
+            <div className={styles.wrapper}>
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className={styles.noResults}>
+              <h2>No movies were found. Please adjust your search term.</h2>
+            </div>
+          )
+        }
       </div>
-    </div>
+    </>
+
   );
 }
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,17 @@
 import styles from '../styles/MovieCard.module.sass';
 
-const MovieCard = () => {
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
   return (
-    <div className={styles.container}></div>
+    <div className={styles.container} key={imdbID}>
+      <div className={styles.poster}>
+        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+      </div>
+      <div className={styles.info}>
+        <p><em>{Type}</em></p>
+        <h3>{Title}</h3>
+        <h4>{Year}</h4>
+      </div>
+    </div>
   )
 }
 
