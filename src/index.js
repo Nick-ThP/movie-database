99ffce4d0eec55348127b75dad13eb3a4f807795
Nicklas <nicklas_tp@hotmail.com>
2022-07-07 17:04:05 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/index.module.sass';
+import './styles/Index.module.sass';
 
 ReactDOM.render(
   <React.StrictMode>
